9f3c3e5812b5cf93ccd08b6a08259003297818f9
ADHAVA4 <80875101+ADHAVA4@users.noreply.github.com>
2024-10-25 12:09:41 +0530

    Update app.py

--- app.py
+++ app.py
@@ -61,6 +61,7 @@ def login_page():
             st.session_state.logged_in = True  # Set a session state variable
             st.session_state.current_page = "Sales"  # Redirect to default module
             st.success("Login successful! Welcome to the dashboard.")
+            sales()
         else:
             st.error("Invalid username or password")
 
